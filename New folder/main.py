20ff3854bd2c655e3053b2ca485ca81679b69ff2
Tiar Hoda <95533050+TiarTuan@users.noreply.github.com>
2022-08-17 09:59:46 +0200

    Add files via upload

---
+++ New folder/main.py
@@ -0,0 +1,27 @@
+# krijimi i nje liste
+
+# Blank list
+vargu = []
+
+# list
+array = [1,2,3]
+
+# list me vlera te ndryshme
+varg3 = [1,"Probit","Academy","2022","Tiar Hoda"]
+print(varg3)
+
+# list me vlera duplikate (te dyfishta)
+sekuence = [1,2,2,3,3]
+
+# list me vlera duplikate te ndryshme
+sekuence2 = [1,1,'Tiar Hoda','Probit','Academy','Tiar Hoda']
+
+# Aksesimi i elementeve ne list
+varg = [1,2,3,4]
+
+
+print(sekuence2)
+print(sekuence)
+print(varg3)
+print(vargu)
+print(array)
